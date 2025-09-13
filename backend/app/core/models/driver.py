a02be414f92c593ff4d1e732b7ec5be025491cee
hian18 <hianaguiar@hotmail.com>
2025-09-13 14:56:14 -0300

    a

--- backend/app/core/models/driver.py
+++ backend/app/core/models/driver.py
@@ -6,18 +6,23 @@ from app.core.models.core import Log
 #     __tablename__: str = "drivers"  # type: ignore
 #     id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': False})
 
-# class Veichele(Log, table=True):
-#     __tablename__: str = "veicheles"  # type: ignore
-#     id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': False})
-#     placa: str = Field(index=True, max_length=20, unique=True)
-#     model: str = Field(index=True, max_length=100)
-#     color: str = Field(index=True, max_length=50)
-#     year: int = Field(index=True)
-#     capacidade: int = Field(index=True)
-#     category: str = Field(index=True, max_length=50)
-#     is_avaliable: bool = Field(default=True)
-    # motorista_id: int | None = Field(default=None, foreign_key="drivers.id")
+class Veichele(Log, table=True):
+    __tablename__: str = "veicheles"  # type: ignore
+    id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': False})
+    placa: str = Field(index=True, max_length=20, unique=True)
+    model: str = Field(index=True, max_length=100)
+    color: str = Field(index=True, max_length=50)
+    year: int = Field(index=True)
+    capacidade: int = Field(index=True)
+    category: str = Field(index=True, max_length=50)
+    is_avaliable: bool = Field(default=True)
+    motorista_id: int | None = Field(default=None, foreign_key="drivers.id")
 
+class ViecheleCategory(Log, table=True):
+    __tablename__: str = "veichele_categories"  # type: ignore
+    id: int = Field(primary_key=True, sa_column_kwargs={'autoincrement': False})
+    name: str = Field(index=True, max_length=50, unique=True)
+    description: str = Field(index=True, max_length=255)
 
 
 # class Ride(Log, table=True):
--- scripts/extract_table.py
+++ scripts/extract_table.py
@@ -7,12 +7,14 @@ tables = camelot.read_pdf('/home/hian/Downloads/MÁSCARA-PBEV-2025-JUL-29 (1).pd
 if tables:
     # Print the number of tables found
     print(f"Number of tables found: {len(tables)}")
-
+    for i, table in enumerate(tables, start=1):
+        if i % 2 == 0:  # Print only even-indexed tables
+            print(f"Table {i}:\n{table.df}\n")
     # Access the first table and convert it to a pandas DataFrame
-    df = tables[0].df
-    print(df)
+    # df = tables[0].df
+    # print(df)
 
     # Export all tables to a CSV file
-    tables.export('output.csv', f='csv', compress=True)
+    # tables.export('output.csv', f='csv', compress=True)
 else:
     print("No tables were found on the specified page.")
